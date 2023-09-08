faadc8665677b7a1bb409d1f8a769881d2baf288
JangHyuck Yun <skg09203@naver.com>
2023-09-08 16:29:24 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.62 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -16,6 +16,8 @@ function plusStoragePeriod(date, period) {
 	return `${year}${type}${addZero(month)}${type}${addZero(day)}`
 }
 
+
+// (유효기간(달) + 수집기간) - 오늘 날짜의 결과값이 0이거나 0 아래일 경우 만기 되었다고 판단.
 function solution(today, terms, privacies) {
 	today = dateToNumber(today);
 	
@@ -30,3 +32,5 @@ function solution(today, terms, privacies) {
 		return acc;
 	}, []);
 }
+
+console.log(solution("2022.05.19",["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]))
