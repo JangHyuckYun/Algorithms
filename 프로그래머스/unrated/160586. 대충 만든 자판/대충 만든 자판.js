9f4139da9f3d8ecae0e087613aee4a4ef7ff99bb
JangHyuck Yun <skg09203@naver.com>
2023-09-08 15:21:33 +0900

    Update 대충 만든 자판.js

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,4 +1,5 @@
 function solution(keymap, targets) {
+	// ["ABC", "BBC"] 의 형태를 { A:1, B:1, C:3 } 의 형태로 변환.
 	const alphaObj = keymap.reduce((acc, arr) => {
 		[...arr].forEach((char, idx) => {
 			idx = idx + 1;
@@ -10,7 +11,11 @@ function solution(keymap, targets) {
 		return acc;
 	}, {});
 	
+	// map을 통해 [["ABC"], ["ABC"]] 등의 형태를 [[1,1,5], [1,1,5]] 등의 형태로 변환
+	// -> reduce를 통해 [[1,1,5], [1,1,5]] 형태를 [9,9] 형태로 변환
 	return targets
 	.map(str => [...str].map(str => alphaObj[str]).reduce((acc, n) => acc + n, 0))
 	.reduce((acc, n) => [...acc, isNaN(n) ? -1 : n], []);
 }
+
+solution(["ABACD", "BCEFD"], ["ABCD","AABB"])
