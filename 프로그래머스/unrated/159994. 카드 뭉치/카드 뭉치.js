556d0337cde2191eae0df24b35f6a981a6f3be89
JangHyuck Yun <skg09203@naver.com>
2023-09-08 15:28:32 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,15 @@
+function solution(cards1, cards2, goal) {
+	return !goal.some(goalWord => {
+		if (cards1[0] === goalWord) {
+			cards1.shift();
+			return false;
+		}
+		
+		if (cards2[0] === goalWord) {
+			cards2.shift();
+			return false;
+		}
+		
+		return true;
+	}) ? "Yes" : "No";
+}
